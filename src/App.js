cd9fb628ec4f35d63c265bd01c15785ddad29159
derek <derekblackj@gmail.com>
2019-07-23 20:52:36 -0400

    Updated css source to be bulma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section class="hero is-info is-large">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            BeatKit
+          </h1>
+          <h2 class="subtitle">
+            A way for artists to create, share, and collaborate on sample packs. Connect with your favorite artists.
+          </h2>
+        </div>
+      </div>
+    </section>
   );
 }
 
