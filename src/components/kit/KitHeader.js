9d33f276948781c37f5ba130e3e25cbe23879fe5
derek <derekblackj@gmail.com>
2019-07-28 13:47:04 -0400

    Added media to card header

--- src/components/kit/KitHeader.js
+++ src/components/kit/KitHeader.js
@@ -4,10 +4,20 @@ import KitDropdown from './KitDropdown'
 function KitHeader(props) {
     return(
         <header className="card-header">
-            <div className="card-header-title">
-                <p>{props.kitName}</p>
+            <div className="media">
+                <figure className="media-left">
+                    <p className="image is-48x48">
+                    <img src="https://bulma.io/images/placeholders/128x128.png"></img>
+                    </p>
+                </figure>
+                <div className="media-content">
+                    <div className="content">
+                        <div className="card-header-title">
+                            <p>{props.kitName}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <KitDropdown />
         </header>
     )
 }
